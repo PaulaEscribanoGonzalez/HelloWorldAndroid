43374058b10abfeab92048359b0657130efe5cf1
PaulaEuneiz <paula.escribano@euneiz.com>
2025-11-18 18:10:30 +0100

    no message

---
+++ HelloWorldAndroid2/app/src/main/java/paula/escribano/helloworldandroid/MainActivity.kt
@@ -0,0 +1,30 @@
+package paula.escribano.helloworldandroid
+
+import android.app.Activity
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import paula.escribano.helloworldandroid.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        //binding.helloWorldTextView.text = "Hola a todos"
+    }
+}
